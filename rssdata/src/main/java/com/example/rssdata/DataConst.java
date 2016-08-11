304348cc51e7e99b45f2c45e013dafae838d1654
demixdn <demixdn@gmail.com>
2016-08-11 17:52:42 +0300

    Initial load android project

---
+++ rssdata/src/main/java/com/example/rssdata/DataConst.java
@@ -0,0 +1,8 @@
+package com.example.rssdata;
+
+/**
+ * Created by Aleksandr on 11.08.2016 in RssFeed.
+ */
+public class DataConst {
+    public static final String RSS_ENDPOINT = "http://www.cbc.ca/cmlink/rss-topstories";
+}
---
+++ rssdata/src/main/java/com/example/rssdata/model/RssItemDTO.java
@@ -0,0 +1,7 @@
+package com.example.rssdata.model;
+
+/**
+ * Created by Aleksandr on 11.08.2016 in RssFeed.
+ */
+public class RssItemDTO {
+}
